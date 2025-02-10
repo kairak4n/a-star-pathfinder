1608ed91e7ea6fac7f4d7f764315eca154e555b9
kairak4n <kshaunkaraca@gmail.com>
2025-02-10 21:42:36 +0300

    Add more constant colors

--- constants.py
+++ constants.py
@@ -2,9 +2,16 @@ HEX_SIZE = 40 # Hex edge length
 WIDTH, HEIGHT = 1200, 800
 
 # Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-BLUE = (100, 100, 255)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
 # Hexagon grid directions (cubic)
 DIRECTIONS = [
